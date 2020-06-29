88a7adda198c29bc4f63334426f9b7c5548b25a5
BonIlcer <pantaev.e@mail.ru>
2020-06-29 21:22:33 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Image Gallery</h1>
+      <div className="gallery">
+        <div className="img-container">
+          <img src={"./img/1.jpg"} alt="img" dataOriginal="./img/1full.jpg" />
+        </div>
+        <div className="img-container">
+          <img src="./img/2.jpg" alt="img" dataOriginal="./img/2full.jpg" />
+        </div>
+        <div className="img-container">
+          <img src="./img/3.jpg" alt="img" dataOriginal="./img/3full.jpg" />
+        </div>
+        <div className="img-container">
+          <img src="./img/4.jpg" alt="img" dataOriginal="./img/4full.jpg" />
+        </div>
+      </div>
+    </>
   );
 }
 
